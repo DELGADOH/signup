b0a90b5f1c606d8385054614f7b220d61fc98145
DELGADOH <145569352+DELGADOH@users.noreply.github.com>
2023-10-26 15:42:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// This is where you can add JavaScript for form interactions and AJAX requests to your backend.
+
+document.getElementById('login-button').addEventListener('click', function() {
+    // Implement login logic here
+});
+
+document.getElementById('signup-button').addEventListener('click', function() {
+    // Implement signup logic here
+});
+
+document.getElementById('logout-button').addEventListener('click', function() {
+    // Implement logout logic here
+});
+
+document.getElementById('reset-button').addEventListener('click', function() {
+    // Implement password reset logic here
+});
